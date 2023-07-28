f7028bc9c2c518bb3d7fc19ec3a5c69b58235ba6
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-28 14:17:00 +0530

    Config the database

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import mysql, {Pool} from 'promise-mysql';
 
 const app = express();
 const router = express.Router();
+
+let pool: Pool;
+
+async function initPool() {
+    pool = await mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        database: 'dep10_simple_task_app',
+        user: 'root',
+        password: 'mysql',
+        connectionLimit: 5,
+    });
+}
+
 
 /*get all task*/
 router.get("/", (req, res) => {
